d3da244352433021728e75017573e356f7a169d6
lisaericksoncoding <111710705+lisaericksoncoding@users.noreply.github.com>
2022-12-08 22:16:48 -0600

    server.js creation

---
+++ server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const { appendFile } = require('fs');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+ 
+const sess = {
+    secret: "super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  });
